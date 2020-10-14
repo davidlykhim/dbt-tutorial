7e33b2cdd07daa33655864bf8cdf442dbcf52fee
David Khim <david@fishtownanalytics.com>
2020-10-14 22:32:36 +0000

    Add stage orders, orders, customers

---
+++ models/orders.sql
@@ -0,0 +1,26 @@
+with
+
+-- Think of this as importing your data
+orders as (
+    select * from {{ ref ('stg_orders')}}
+),
+
+payments as (
+    select * from {{ ref ('stg_payments')}}
+),
+
+
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        sum(payments.amount) as amount
+    from orders
+    left join payments using (order_id)
+      -- can also use > left join payments on payments.order_id = orders.order_id
+    where payments.status = 'success'
+    group by 1, 2, 3
+)
+
+select * from final
---
+++ models/stg_payments.sql
@@ -0,0 +1,10 @@
+select 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount, -- convert from cents to dollars
+    created as created_at
+from raw.stripe.payment
+-- Look at raw.stripe.payments (amount ??, 1:many for orders:payments,)
+-- create a staging layer
